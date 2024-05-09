8bc3bd29655b3bca75599db032afd4bd13613cb7
kalyanvalisetty <139623608+kalyanvalisetty@users.noreply.github.com>
2024-05-09 11:14:13 +0530

    Project Moviebuzz

---
+++ script.js
@@ -0,0 +1,16 @@
+function chatrapathi() {
+  document.getElementById("actor").src = "https://files.prokerala.com/downloads/pics/800/prabhas-46685.jpg";
+  document.getElementById("actress").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ1BdD7K8E1mEZDYx86FCFHpzmq8JL5SBXnsPFbG0HNQ&s";
+  document.getElementById("director").src = "https://www.bollywoodhungama.com/wp-content/uploads/2016/03/S.S.-Rajamouli-banner.jpg";
+}
+function bahubali() {
+    document.getElementById("actor").src = "https://c4.wallpaperflare.com/wallpaper/741/642/330/8k-baahubali-2-the-conclusion-prabhas-4k-wallpaper-preview.jpg";
+    document.getElementById("actress").src = "https://w0.peakpx.com/wallpaper/79/538/HD-wallpaper-anushka-shetty-anushka-shetty-actress-women-thumbnail.jpg";
+    document.getElementById("director").src = "https://www.bollywoodhungama.com/wp-content/uploads/2016/03/S.S.-Rajamouli-banner.jpg";
+}
+
+function projectK() {
+    document.getElementById("actor").src = "https://www.m9.news/wp-content/uploads/2023/07/Project-K-Kalki-2898-AD-Glimpse.jpg";
+    document.getElementById("actress").src = "https://images.news18.com/ibnlive/uploads/2023/07/deepika-padukone-in-project-k-16896462663x2.jpg?impolicy=website&width=640&height=480";
+    document.getElementById("director").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS39zCwnTDC1h0miZf1N2InfGB6afrUmdwbLmo_oVsU2w&s";
+}
